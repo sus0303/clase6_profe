76ccb7d34af9a22ecad5e0a64c4cdb2e5e6b4454
sus0303 <susanahlopez3@gmail.com>
2026-02-26 11:15:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,10 +8,10 @@ from PIL import Image
 import base64
 
 st.title("Conversión de Texto a Audio")
-image = Image.open('gato_raton.png')
+image = Image.open('moon.png')
 st.image(image, width=350)
 with st.sidebar:
-    st.subheader("Esrcibe y/o selecciona texto para ser escuchado.")
+    st.subheader("Esrcibe tu texto para ser escuchado.")
 
 
 try:
@@ -20,17 +20,14 @@ except:
     pass
 
 st.subheader("Una pequeña Fábula.")
-st.write('¡Ay! -dijo el ratón-. El mundo se hace cada día más pequeño. Al principio era tan grande que le tenía miedo. '  
-         ' Corría y corría y por cierto que me alegraba ver esos muros, a diestra y siniestra, en la distancia. ' 
-         ' Pero esas paredes se estrechan tan rápido que me encuentro en el último cuarto y ahí en el rincón está '  
-         ' la trampa sobre la cual debo pasar. Todo lo que debes hacer es cambiar de rumbo dijo el gato...y se lo comió. ' 
-         '  '
-         ' Franz Kafka.'
+st.write('¡La ignorancia es la noche de la mente: pero una noche sin luna y sin estrellas.' 
+         ' '
+         'Confucio'
         
         )
            
 st.markdown(f"Quieres escucharlo?, copia el texto")
-text = st.text_area("Ingrese El texto a escuchar.")
+text = st.text_area("Ingrese tu texto para escucharlo.")
 
 tld='com'
 option_lang = st.selectbox(
